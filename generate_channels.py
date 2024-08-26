1c946715918fbd202f8cc43cd9afb0ad18d38cd5
MavijaviGit <cornerphc2@gmail.com>
2024-08-26 21:22:12 +0800

    Update generate_channels.py

--- generate_channels.py
+++ generate_channels.py
@@ -35,7 +35,7 @@ def save_unique_channels(unique_channels, output_file):
 
 base_directory = 'sites'
 output_file = 'updated_channels.xml'
-target_dirs = ['tvguide.myjcom.jp', 'sky.com]
+target_dirs = ['tvguide.myjcom.jp','sky.com]
 
 unique_channels = collect_unique_channels(base_directory, target_dirs)
 save_unique_channels(unique_channels, output_file)
